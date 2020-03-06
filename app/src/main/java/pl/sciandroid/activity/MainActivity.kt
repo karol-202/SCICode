47a4f845e3d5ecec13447d96a8448a8729200009
karol <karoljurski1@gmail.com>
2020-03-06 21:48:13 +0100

    Prepare for workshops

--- app/src/main/java/pl/sciandroid/activity/MainActivity.kt
+++ app/src/main/java/pl/sciandroid/activity/MainActivity.kt
@@ -1,45 +1,14 @@
 package pl.sciandroid.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
-import org.koin.android.ext.android.get
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import androidx.appcompat.app.AppCompatActivity
 import pl.sciandroid.R
-import pl.sciandroid.adapter.NoteAdapter
-import pl.sciandroid.database.dao.NoteDao
-import pl.sciandroid.model.Note
-import pl.sciandroid.viewmodel.NoteViewModel
 
 class MainActivity : AppCompatActivity()
 {
-	private val noteViewModel by viewModel<NoteViewModel>()
-
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
-
-		val noteAdapter = NoteAdapter(this)
-		noteAdapter.setOnNoteDeleteListener {
-			noteViewModel.removeNote(it)
-		}
-
-		noteViewModel.notes.observe(this, Observer {
-			noteAdapter.notes = it
-		})
-
-		recyclerNotes.layoutManager = LinearLayoutManager(this)
-		recyclerNotes.adapter = noteAdapter
-
-		buttonNoteAdd.setOnClickListener {
-			val content = editTextNote.text.toString()
-			noteViewModel.addNote(content)
-
-			editTextNote.text.clear()
-		}
 	}
 }
